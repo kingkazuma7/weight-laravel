f85a27ce3e2e3c60e266e64e5ffe39e2e063e62a
kingkazuma7 <your.nasionasio7@gmail.com>
2025-09-30 18:12:25 +0900

    体重クラスモデルにファクトリを追加し、マスアサインメントを許可するカラムを定義

--- app/Models/WeightClass.php
+++ app/Models/WeightClass.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class WeightClass extends Model
 {
-    //
+  use HasFactory;
+  
+  // マスアサインメントで挿入を許可するカラム
+  protected $fillable = [
+    'type',
+    'name',
+    'weight_limit',
+  ];
 }
