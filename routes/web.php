899b01b87d90a48743823a8851019eee17bd2a43
kingkazuma7 <your.nasionasio7@gmail.com>
2025-09-30 18:17:36 +0900

    WeightClassControllerをインポートし、'/weight-classes'へのGETリクエストに対してindexメソッドを実行する新しいルートを定義

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WeightClassController; // ★ 1. コントローラをインポート
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ★ 2. 新しいルートを定義
+// GETリクエストで'/weight-classes'にアクセスが来たら、
+// WeightClassControllerのindexメソッドを実行する
+Route::get('/weight-classes', [WeightClassController::class, 'index']);
