4d5338f828541dbd322fbbc6d94fe133b57c64bd
kingkazuma7 <your.nasionasio7@gmail.com>
2025-09-30 18:13:33 +0900

    体重クラスシーダーを更新し、データの重複挿入を防ぐためにデータをクリアする処理を追加。ボクシング、RIZIN、UFCの全階級と体重制限をデータベースに挿入するロジックを実装。

--- database/seeders/WeightClassSeeder.php
+++ database/seeders/WeightClassSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\WeightClass; // モデルをインポート
 
 class WeightClassSeeder extends Seeder
 {
@@ -12,6 +12,62 @@ class WeightClassSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // データの重複挿入を防ぐため、毎回データをクリア
+        WeightClass::truncate();
+
+        // 挿入する全データ構造
+        $data = [
+            'ボクシング' => [
+                ['name' => 'ミニマム級', 'weight_limit' => '47.62kg'],
+                ['name' => 'ライトフライ級', 'weight_limit' => '48.97kg'],
+                ['name' => 'フライ級', 'weight_limit' => '50.80kg'],
+                ['name' => 'スーパーフライ級', 'weight_limit' => '52.16kg'],
+                ['name' => 'バンタム級', 'weight_limit' => '53.52kg'],
+                ['name' => 'スーパーバンタム級', 'weight_limit' => '55.34kg'],
+                ['name' => 'フェザー級', 'weight_limit' => '57.15kg'],
+                ['name' => 'スーパーフェザー級', 'weight_limit' => '58.97kg'],
+                ['name' => 'ライト級', 'weight_limit' => '61.23kg'],
+                ['name' => 'スーパーライト級', 'weight_limit' => '63.50kg'],
+                ['name' => 'ウエルター級', 'weight_limit' => '66.68kg'],
+                ['name' => 'スーパーウエルター級', 'weight_limit' => '69.85kg'],
+                ['name' => 'ミドル級', 'weight_limit' => '72.57kg'],
+                ['name' => 'スーパーミドル級', 'weight_limit' => '76.20kg'],
+                ['name' => 'ライトヘビー級', 'weight_limit' => '79.38kg'],
+                ['name' => 'クルーザー級', 'weight_limit' => '90.72kg'],
+                ['name' => 'ヘビー級', 'weight_limit' => '90.72kg超'],
+            ],
+            'RIZIN(MMA)' => [
+                ['name' => 'フライ級', 'weight_limit' => '57 kg'],
+                ['name' => 'バンタム級', 'weight_limit' => '61 kg'],
+                ['name' => 'フェザー級', 'weight_limit' => '66 kg'],
+                ['name' => 'ライト級', 'weight_limit' => '71 kg'],
+                ['name' => 'ウェルター級', 'weight_limit' => '77 kg'],
+                ['name' => 'ミドル級', 'weight_limit' => '85 kg'],
+                ['name' => 'ライトヘビー級', 'weight_limit' => '95 kg'],
+                ['name' => 'ヘビー級', 'weight_limit' => '120 kg'],
+                ['name' => '無差別級', 'weight_limit' => '無制限'],
+            ],
+            'UFC(MMA)' => [
+                ['name' => 'フライ級', 'weight_limit' => '56.7 kg'],
+                ['name' => 'バンタム級', 'weight_limit' => '61.2 kg'],
+                ['name' => 'フェザー級', 'weight_limit' => '65.8 kg'],
+                ['name' => 'ライト級', 'weight_limit' => '70.3 kg'],
+                ['name' => 'ウェルター級', 'weight_limit' => '77.1 kg'],
+                ['name' => 'ミドル級', 'weight_limit' => '83.9 kg'],
+                ['name' => 'ライトヘビー級', 'weight_limit' => '93.0 kg'],
+                ['name' => 'ヘビー級', 'weight_limit' => '120.2 kg'],
+            ],
+        ];
+
+        // データのループ処理と挿入
+        foreach ($data as $type => $classes) {
+            foreach ($classes as $class) {
+                WeightClass::create([
+                    'type' => $type,
+                    'name' => $class['name'],
+                    'weight_limit' => $class['weight_limit'],
+                ]);
+            }
+        }
     }
 }
