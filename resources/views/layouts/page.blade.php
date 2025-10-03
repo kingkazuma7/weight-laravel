637929ad2d7eeb6561cced23989cd4cb77eb1873
kingkazuma7 <your.nasionasio7@gmail.com>
2025-10-03 13:38:52 +0900

    CSSファイルのリンクをsecure_assetに変更し、weight-classes.cssを新たに追加しました。

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -41,7 +41,8 @@
         <link rel="icon" href="{{ asset('images/logo.webp') }}" type="image/x-icon">
 
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/weight-classes.css') }}" rel="stylesheet">
         <script src="https://cdn.tailwindcss.com"></script>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
     </head>
